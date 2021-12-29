77562a2ba2ea5dd9735f67cbcbd368d9bf81b15d
Xavier <nicodemexavier@gmail.com>
2021-12-29 16:11:19 +0100

    create category and program entity and relation between

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
@@ -17,4 +20,15 @@ class ProgramController extends AbstractController
             'website' => 'Wild Series',
         ]);
     }
+    /**
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     */
+    public function show(int $id)
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id
+        ]);
+
+    }
+
 }
